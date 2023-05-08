253562a31c56609a2dbdb3206eaf55deea1bc669
HackerBurscht <noe.kraettli@gmx.ch>
2023-05-08 16:33:58 +0200

    Added modal and over prototype.

--- app.js
+++ app.js
@@ -283,3 +283,30 @@ document.getElementById("yearInput").addEventListener('keypress', function (pres
 document.getElementById("timelineID").addEventListener("wheel", scroll_event, { passive: false });
 
 
+//EH for modal https://www.freecodecamp.org/news/how-to-build-a-modal-with-javascript/
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const openModalBtn = document.querySelector(".btn-open");
+const closeModalBtn = document.querySelector(".btn-close");
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+openModalBtn.addEventListener("click", openModal);
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+closeModalBtn.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal)
+//document.addEventListener("keydown");
+
+/* document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    modalClose();
+  }
+}); */
